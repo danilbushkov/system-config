c9edfd1c8dcaa052db9ab44054cd68f2fb8962fc
danilbushkov <danil1357997@gmail.com>
2024-10-07 12:10:25 +0300

    add install script

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,19 +1,31 @@
-{
+{config, pkgs, ...}: 
+let
+#nvim = builtins.fetchGit {
+#  url = "https://github.com/danilbushkov/nvim-config.git";
+#  ref = "refs/heads/main";
+#};
+  nvim = pkgs.fetchFromGitHub {
+    owner = "danilbushkov";
+    repo = "nvim-config";
+    rev = "refs/heads/main";
+  };
+in {
   home = {
 
-	username = "danil";
-	homeDirectory = "/home/danil";
-  	stateVersion = "24.05";
+  username = "danil";
+  homeDirectory = "/home/danil";
+    stateVersion = "24.05";
   };
 
   programs.home-manager.enable = true;
   programs.git = {
-	enable = true;
-	userName = "danilbushkov";
-	userEmail = "danil1357997@gmail.com";
+  enable = true;
+  userName = "danilbushkov";
+  userEmail = "danil1357997@gmail.com";
   };
 
-
-
-
+#  home.file."${config.xdg.configHome}/nvim" = {
+#    source = "${nvim}";
+#  };
+  
 }
