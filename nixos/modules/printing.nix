441966f4244ee0ac41122b05c40ec6232793d8ee
danilbushkov <danil1357997@gmail.com>
2024-10-20 18:55:38 +0300

    add share printing

--- nixos/modules/printing.nix
+++ nixos/modules/printing.nix
@@ -2,5 +2,10 @@
   services.printing = {
     enable = true;
     drivers = with pkgs; [ gutenprint ];
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
   };
 }
--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -2,7 +2,8 @@
   users.users.danil = {
     isNormalUser = true;
     description = "danil";
-    extraGroups = [ "networkmanager" "wheel" "audio" "bluetooth" "docker" ];
+    extraGroups =
+      [ "networkmanager" "wheel" "audio" "bluetooth" "docker" "lpadmin" ];
     # packages = with pkgs; [];
   };
 }
