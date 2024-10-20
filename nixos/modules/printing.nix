8e0734b87a18421821c1e703d4c4ea59006b4f18
danilbushkov <danil1357997@gmail.com>
2024-10-20 20:10:54 +0300

    delete printing shared

--- nixos/modules/printing.nix
+++ nixos/modules/printing.nix
@@ -2,10 +2,5 @@
   services.printing = {
     enable = true;
     drivers = with pkgs; [ gutenprint ];
-    listenAddresses = [ "*:631" ];
-    allowFrom = [ "all" ];
-    browsing = true;
-    defaultShared = true;
-    openFirewall = true;
   };
 }
