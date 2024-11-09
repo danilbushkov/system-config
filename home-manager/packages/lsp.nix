dc2898761705a22e8bd75ffe35f2a970eb87c686
danilbushkov <danil1357997@gmail.com>
2024-11-09 17:17:59 +0300

    add lsp for typescript

--- home-manager/packages/lsp.nix
+++ home-manager/packages/lsp.nix
@@ -1,1 +1,10 @@
-pkgs: with pkgs; [ vscode-langservers-extracted lua-language-server texlab ]
+pkgs:
+with pkgs; [
+  vscode-langservers-extracted
+  lua-language-server
+  texlab
+
+  nodePackages_latest.typescript-language-server
+  nodePackages_latest.eslint
+  prettierd
+]
