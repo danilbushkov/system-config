0e75228e5ac5096a1926779e867359e20ef4f081
danilbushkov <danil1357997@gmail.com>
2025-05-03 13:12:04 +0300

    add hyprlock settings

--- home-manager/modules/window-manager/default.nix
+++ home-manager/modules/window-manager/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   wayland.windowManager.hyprland = import ./hyprland;
 
   services.hypridle = import ./hypridle;
 
-  programs.hyprlock = import ./hyprlock;
+  programs.hyprlock = import ./hyprlock config;
 
   home.packages = with pkgs; [
     wofi
--- home-manager/modules/window-manager/hyprlock/default.nix
+++ home-manager/modules/window-manager/hyprlock/default.nix
@@ -1,1 +1,25 @@
-{ enable = true; }
+config: {
+  enable = true;
+  settings = {
+    general = {
+      grace = 1;
+      hide_cursor = true;
+    };
+
+    background = {
+      monitor = "";
+      path = "${config.home.homeDirectory}/Pictures/wallpapers/bodyOfWater.jpg";
+      brightness = 0.3;
+      blur_size = 2;
+      blur_passes = 4;
+    };
+
+    input-field = [{
+      monitor = "";
+      size = "200, 50";
+      hide_input = false;
+      outline_thickness = 1;
+      placeholder_text = "Password...";
+    }];
+  };
+}
