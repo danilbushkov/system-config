4d9eb2feff6d402adab9d17d335499c6b64cdb59
danilbushkov <danil1357997@gmail.com>
2025-05-03 14:57:46 +0300

    add listeners for timeouts

--- home-manager/modules/window-manager/hypridle/default.nix
+++ home-manager/modules/window-manager/hypridle/default.nix
@@ -9,7 +9,15 @@
         on-timeout = "brightnessctl s 10% && ddcutil setvcp 10 10";
         on-resume = "brightnessctl s 30% && ddcutil setvcp 10 30";
       }
-      { }
+      {
+        timeout = 600;
+        on-timeout = "hyprlock";
+      }
+      {
+        timeout = 900;
+        on-timeout = "hyprctl dispatch dpms off";
+        on-resume = "hyprctl dispatch dpms on";
+      }
     ];
   };
 }
