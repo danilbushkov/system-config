d04b44c1418d22b113b6fa838788d37aa468eff2
danilbushkov <danil1357997@gmail.com>
2025-05-04 18:06:12 +0300

    disable dmps off

--- home-manager/modules/window-manager/hypridle/default.nix
+++ home-manager/modules/window-manager/hypridle/default.nix
@@ -13,11 +13,11 @@
         timeout = 600;
         on-timeout = "hyprlock";
       }
-      {
-        timeout = 900;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
+      #      {
+      #        timeout = 900;
+      #        on-timeout = "hyprctl dispatch dpms off";
+      #        on-resume = "hyprctl dispatch dpms on";
+      #      }
     ];
   };
 }
