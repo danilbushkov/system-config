b35f175059ee8c290bab49974d212f39b3c5ac02
danilbushkov <danil1357997@gmail.com>
2025-09-13 20:57:49 +0300

    add initExtra for bash

--- home-manager/modules/console/default.nix
+++ home-manager/modules/console/default.nix
@@ -3,7 +3,14 @@
   imports = [ ./helix.nix ./starship.nix ./git.nix ./rust.nix ];
 
   programs = {
-    bash.enable = true;
+    bash = {
+      enable = true;
+      initExtra = ''
+        if [ -f ~/.profile ]; then
+          . ~/.profile
+        fi
+      '';
+    };
     gh.enable = true;
     zellij.enable = true;
   };
