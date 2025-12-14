346cefab61753eae9ebe7e991604d7c91367569a
danilbushkov <danil1357997@gmail.com>
2025-12-14 15:15:01 +0300

    add wallpapers

--- home-manager/wallpapers.nix
+++ home-manager/wallpapers.nix
@@ -1,11 +1,6 @@
 { config, ... }: {
 
   home.file."${config.home.homeDirectory}/Pictures/wallpapers/bodyOfWater.jpg".source =
-    builtins.fetchurl {
-      url =
-        "https://unsplash.com/photos/iB7gjOsLrEQ/download?ixid=M3wxMjA3fDB8MXxhbGx8NDF8fHx8fHx8fDE3MjgzMTcwNzd8&force=true";
-      sha256 = "1w0ihvj4v4lvbz7n1a15advn25xkyrs6q5m60xi68h7s0wvbx5yh";
-      name = "bodyOfWater";
-    };
+    ../wallpapers/bodyOfWater.jpg;
 
 }
